90cbbe943dc6c0f3b3802e20f15e406fde5b946c
Cole Wangler <cole.wangler@ndsu.edu>
2023-11-30 17:58:59 -0600

    Uploading the Assignment

---
+++ src/Customer.java
@@ -0,0 +1,128 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+import java.sql.*;
+import java.text.DecimalFormat;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Cole Wangler
+ */
+public class Customer {
+    private static String url = "jdbc:postgresql://localhost:5432/ShoppingCart";
+    private static String user = "postgres";
+    private static String password = "ColeyJ56!";  
+    
+    
+    public static void addCustomer() {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(url, user, password);
+            Statement statement = conn.createStatement();
+
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Please enter the first name: ");
+            String firstName = scan.nextLine();
+            System.out.println("Please enter the last name: ");
+            String lastName = scan.nextLine();
+            System.out.println("Please enter the phone number: ");
+            String phoneNumber = scan.nextLine();
+            System.out.println("Please enter the email: ");
+            String email = scan.nextLine();
+            System.out.println("Please enter the address: ");
+            String address = scan.nextLine();
+
+            String insertIntoCustomer = "INSERT INTO customer (customer_first_name, customer_last_name, customer_phone_number, customer_email, current_address) "
+                    + "Values ('Peter', 'Mulark', '1-320-987-9898', 'customer4@email.com', '2340 23rd St Fargo')";
+//                    + "VALUES ('?','?','?','?','?')";
+//
+//            PreparedStatement pstmt = conn.prepareStatement(insertIntoCustomer);
+//            pstmt.setString(1, firstName);
+//            pstmt.setString(2, lastName);
+//            pstmt.setString(3, phoneNumber);
+//            pstmt.setString(4, email);
+//            pstmt.setString(5, address);
+
+            int res = statement.executeUpdate(insertIntoCustomer);
+
+            System.out.println(res + " records inserted");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Cannot Load Driver");
+        } catch (SQLException sqle) {
+            System.out.println(sqle.toString());
+        }
+    }
+    
+    public static void getItemsInCart(int x){
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(url, user, password);
+            Statement statement = conn.createStatement();
+
+            DecimalFormat moneyFormat = new DecimalFormat("$0.00");
+
+            String getItemsInCart = "Select item_quantity, item_name, description, cost  From items "
+                    + "Join has_in_list ON has_in_list.item_id = items.item_id "
+                    + "Where has_in_list.customer_id = " + x;
+            ResultSet rs = statement.executeQuery(getItemsInCart);
+
+            int itemNumber = 1;
+
+            while (rs.next()) {
+
+                System.out.println("#" + itemNumber + " Item On List");
+
+                System.out.println("Quantity: " + rs.getInt(1));
+                System.out.println("Item Name: " + rs.getString(2));
+                System.out.println("Item Description: " + rs.getString(3));
+                System.out.println("Item Cost: " + moneyFormat.format(rs.getDouble(4)));
+                System.out.println("----------------------\n");
+
+                itemNumber++;
+
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("Cannot Load Driver");
+        } catch (SQLException sqle) {
+            System.out.println(sqle.toString());
+        }
+    }
+    
+    public static void totalCostOfCart(int x) {
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(url, user, password);
+            Statement statement = conn.createStatement();
+
+            String getTotalCostInCart = "Select item_quantity, cost, amount_off  From items "
+                    + "Join has_in_list ON has_in_list.item_id = items.item_id "
+                    + "Join discounts ON discounts.item_id = items.item_id "
+                    + " Where has_in_list.customer_id = " + x;
+            ResultSet rs = statement.executeQuery(getTotalCostInCart);
+
+            DecimalFormat moneyFormat = new DecimalFormat("$0.00");
+
+            double totalCost = 0;
+            while (rs.next()) {
+                totalCost += rs.getInt(1) * (rs.getDouble(2) - rs.getDouble(3));
+            }
+            System.out.println("Your Total Cost: " + moneyFormat.format(totalCost));
+        } catch (ClassNotFoundException e) {
+            System.out.println("Cannot Load Driver");
+        } catch (SQLException sqle) {
+            System.out.println(sqle.toString());
+        }
+    }
+}
---
+++ src/DatabaseFinal.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+import java.sql.*;
+import java.text.DecimalFormat;
+import java.util.Calendar;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Cole Wangler
+ */
+public class DatabaseFinal {
+
+    private static String url = "jdbc:postgresql://localhost:5432/ShoppingCart";
+    private static String user = "postgres";
+    private static String password = "ColeyJ56!";
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner scan = new Scanner(System.in);
+
+        boolean exit = false;
+        
+        do{
+            System.out.println("------Welcome to the shopping cart Application!-----");
+            int menuSelection;
+            boolean validOption = false;
+            do{
+                    System.out.println("Please Enter 1 to Add A New Customer: ");
+                    System.out.println("Please Enter 2 to View Items in a customers cart: ");
+                    System.out.println("Please enter 3 to Get Total Cost of a customers cart:");
+                    System.out.println("Please press 4 to Exit the Application: ");
+                    menuSelection = scan.nextInt();
+                    
+                    if( menuSelection == 1 || menuSelection == 2 || menuSelection == 3 || menuSelection == 4 )
+                        validOption = true;
+                    else
+                        System.out.println("Not a valid option.");
+            } while (validOption == false);
+            
+            Customer customer = new Customer();
+            int customerID;
+            switch (menuSelection) {
+                case 1:
+                    customer.addCustomer();
+                    break;
+                case 2:
+                    System.out.println("Please enter customer ID: ");
+                    customerID = scan.nextInt();
+                    customer.getItemsInCart(customerID);
+                    break;
+                case 3:
+                    System.out.println("Please enter customer ID: ");
+                    customerID = scan.nextInt();
+                    customer.totalCostOfCart(customerID);
+                    break;
+                case 4:
+                    exit = true;
+                    break;
+            }
+        } while (exit == false);
+    }
+}
+   
+            
+//            String getCustomerInfo = "Select * FROM customer";
+//            
+//            ResultSet rs = statement.executeQuery(getCustomerInfo);
+//            while(rs.next()){
+//                System.out.println("First Name: " + rs.getString(2));
+//                System.out.println("Last Name: " + rs.getString(3));
+//                System.out.println("Phone Number: " + rs.getString(4));
+//                System.out.println("Email: " + rs.getString(5));
+//                System.out.println("Address: " + rs.getString(6));
+//                System.out.println("------------------------------\n");
