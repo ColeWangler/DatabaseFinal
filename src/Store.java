8751e0548a624f2475eea8855fcff335caf631dd
ahorner54 <ahorner589@gmail.com>
2023-12-02 21:33:14 -0600

    Added Address.java with getters and toString()

--- src/Store.java
+++ src/Store.java
@@ -1,6 +1,7 @@
 /**
  *
  * @author andrew.horner
+ * @version 2023.12.02
  */
 public class Store {
     
@@ -8,7 +9,7 @@ public class Store {
     private static String store_name;
     private static Address store_address;
     private static int addresss_id;
-    private static int store_address;
+    private static int store_address_id;
     
     
     
